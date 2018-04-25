e715eafce16a81545d63cc731d107228a0eb0297
Masaya Takahashi <masaya.oor710@gmail.com>
2018-04-25 19:22:31 +0900

    Refactoring controllers(Add & Set check_user before_action)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  def check_user
+    @user = User.find_by(account_id: params[:account_id])
+    unless @user
+      flash[:danger] = "そのページは存在しません"
+      redirect_to current_user
+    end
+  end
+
   private
 
     def logged_in_user
