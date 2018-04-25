59e5ee8c6983e8135117f7b60af52d987a7e2189
Masaya Takahashi <masaya.oor710@gmail.com>
2018-04-25 19:48:39 +0900

    Modify views(S[search])

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  def check_user
-    @user = User.find_by(account_id: params[:account_id])
-    unless @user
-      flash[:danger] = "そのページは存在しません"
-      redirect_to current_user
-    end
-  end
-
   private
 
     def logged_in_user
       unless logged_in?
         flash[:danger] = "ログインしてください！"
         redirect_to login_url
+      end
+    end
+
+    def check_user
+      @user = User.find_by(account_id: params[:account_id])
+      unless @user
+        flash[:danger] = "そのページは存在しません"
+        redirect_to current_user
       end
     end
 
