1358d1f80b898f457da7932048a83a40f5dc7080
Masaya Takahashi <masaya.oor710@gmail.com>
2018-04-30 15:06:14 +0900

    Add params(U,M), method(U,M) about check reset

--- db/migrate/20180425164427_create_mylists.rb
+++ db/migrate/20180425164427_create_mylists.rb
@@ -5,6 +5,8 @@ class CreateMylists < ActiveRecord::Migration[5.1]
       t.references :list,   foreign_key: true
       t.boolean    :active, default: true
       t.boolean    :check,  default: false
+      t.integer    :check_count, default: 0
+      t.integer    :running_days, default: 0
 
       t.timestamps
     end
