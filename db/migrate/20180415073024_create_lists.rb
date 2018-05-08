54d67bdf2f4cb27be49999a6c5dff055eb6f156b
Masaya Takahashi <masaya.oor710@gmail.com>
2018-05-08 16:39:51 +0900

    Modify params(user_id, reference(+fk) => integer(-fk)), action(M[destroy])

--- db/migrate/20180415073024_create_lists.rb
+++ db/migrate/20180415073024_create_lists.rb
@@ -1,13 +1,13 @@
 class CreateLists < ActiveRecord::Migration[5.1]
   def change
     create_table :lists do |t|
-      t.references :user,       null:        false,
-                                foreign_key: true
-      t.string     :content,    null:        false
-      t.integer    :user_count, null:        false,
-                                default:     0
+      t.integer :user_id,    null:    false
+      t.string  :content,    null:    false
+      t.integer :user_count, null:    false,
+                             default: 0
 
       t.timestamps
     end
+    add_index :lists, :user_id
   end
 end
