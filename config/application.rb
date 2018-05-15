acf0d16bb246da97eff67fa5e3a20e391bcf08ba
Masaya Takahashi <masaya.oor710@gmail.com>
2018-05-15 18:26:45 +0900

    Set domain,heroku aal & Add initializers(rack_rewrite)

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Nodobotoke
+module Nodopou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.i18n.default_locale = :ja
