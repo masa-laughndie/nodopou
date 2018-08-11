27f053b7e51a527088b0530ab92dfbcf20723fdf
Masaya Takahashi <masaya.oor710@gmail.com>
2018-08-11 18:23:39 +0900

    Ref: searches_controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,13 +4,9 @@ class SearchesController < ApplicationController
 
   def search
     @user = current_user
-    # @users = User.search(params[:keyword]).page(params[:page])
     @lists = List.search(params[:keyword]).order(user_count: :desc)
     @lists_count = @lists.size
     @lists = @lists.page(params[:page]).per(30)
-    unless params[:keyword].nil?
-      @cuser_list_ids = current_user.mylists.includes(:list).pluck(:list_id)
-    end
+    @cuser_list_ids = current_user.mylists.includes(:list).pluck(:list_id) if params[:keyword].present?
   end
-
 end
