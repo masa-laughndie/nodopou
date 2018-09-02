a77d119d14a9b98ae7d809da452571a957f1555e
Masaya Takahashi <masaya.oor710@gmail.com>
2018-09-03 00:48:42 +0900

    Setting webpack and tsconfig

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+<<<<<<< HEAD
   entry: {
     "frontend/app": "./src/javascripts/index.tsx"
   },
@@ -8,6 +9,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].js"
+=======
+  entry: "./src/javascripts/index.tsx",
+
+  output: {
+    path: path.resolve(__dirname, "../app/assets/javascripts"),
+    filename: "bundle.js"
+  },
+
+  // devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+>>>>>>> 0920fad... Setting webpack and tsconfig
   },
 
   // devtool: "source-map",
