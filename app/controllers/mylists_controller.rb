34db3448611915167273646828d918947bf1ee88
masa-laughndie <masaya.oor710@gmail.com>
2018-12-08 19:05:35 +0900

    add gon.mylists to mylists#index

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -16,6 +16,7 @@ class MylistsController < ApplicationController
     else
       @cuser_list_ids = []
     end
+    gon.mylists = Mylist.find_format_gon_params(@mylists.ids.uniq)
   end
 
   def show
