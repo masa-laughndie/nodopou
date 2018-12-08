65307ed476274999292a8536b5ab48a749cf6931
masa-laughndie <masaya.oor710@gmail.com>
2018-12-08 19:20:38 +0900

    Add Mylist type to window,commonTypes

---
+++ frontend/src/javascripts/types/commonTypes.ts
@@ -0,0 +1,11 @@
+export interface Mylist {
+  id: number;
+  user_id: number;
+  list_id: number;
+  active: boolean;
+  check: boolean;
+  strong: boolean;
+  check_count: number;
+  running_days: number;
+  max_running_days: number;
+}
---
+++ frontend/src/javascripts/types/window.ts
@@ -0,0 +1,15 @@
+export interface GonMylist {
+  id: number;
+  user_id: number;
+  list_id: number;
+  active: boolean;
+  check: boolean;
+  strong: boolean;
+  check_count: number;
+  running_days: number;
+  max_running_days: number;
+}
+
+export interface Gon {
+  mylists: GonMylist[];
+}
