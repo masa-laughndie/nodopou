4476954171a689cefb4d308cd1a48f08e8ede8dc
masa-laughndie <masaya.oor710@gmail.com>
2019-08-18 15:39:22 +0900

    remove rack_rewrite

--- config/initializers/rack_rewrite.rb
+++
@@ -1,7 +0,0 @@
-if ENV['RACK_ENV'] == 'production'
-  Nodopou::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'https://nodopou.herokuapp.com$&', :if => Proc.new { |rack_env|
-      rack_env['SERVER_NAME'] == 'www.nodopou.com'
-    }
-  end
-end
